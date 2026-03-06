4c49763b624ef7bd6f0b642f926545b16919b439
Maria José Sosa <maria.sosa15@eia.edu.co>
2026-03-06 15:52:12 -0500

    Base para concesionario

--- Cardealership_proyecto/src/modelo/Concesionario.java
+++ Cardealership_proyecto/src/modelo/Concesionario.java
@@ -1,5 +1,36 @@
 package modelo;
 
+import modelo.vehiculo.*;
+import modelo.persona.*;
+import modelo.venta.*;
+
 public class Concesionario {
+
+	private String nombre;
+	private Cliente clientes[] = new Cliente[0];
+	private Empleado empleados[] = new Empleado[0];
+	private Vehiculo vehiculos[] = new Vehiculo[0];
+	private Venta ventas[] = new Venta[0];
+
+	public Concesionario(String nombre) {
+		if (nombre.isEmpty() || nombre == null) {
+			throw new IllegalArgumentException("Ingrese un nombre válido para el concesionario");
+		}
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		if (nombre.isEmpty() || nombre == null) {
+			throw new IllegalArgumentException("Ingrese un nombre válido para el concesionario");
+		}
+		this.nombre = nombre;
+	}
+
+	// No hacer getter y setters de los arrays, esos llevan otros nombres
+	// especificados en el diagrama, además de funcionalidades extra
 
 }
