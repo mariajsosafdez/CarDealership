a4d3586dd66f8eae7a0eccbc422cce0cc4502037
Ana Henao <anahenao719@gmail.com>
2026-03-05 21:33:01 -0500

    Actualizo clases

--- Cardealership_proyecto/src/modelo/persona/Empleado.java
+++ Cardealership_proyecto/src/modelo/persona/Empleado.java
@@ -1,5 +1,34 @@
 package modelo.persona;
 
-public class Empleado {
+public class Empleado extends Persona {
+    
+    private float salario;
 
+    public Empleado(String tipoDocumento, String numeroDocumento, String nombre, String apellido, String telefono, float salario) {
+        super(tipoDocumento, numeroDocumento, nombre, apellido, telefono);
+        setSalario(salario); 
+    }
+
+   
+    public float getSalario() {
+        return salario;
+    }
+
+   
+    public void setSalario(float salario) {
+        if (salario < 0) {
+            throw new IllegalArgumentException("El salario no puede ser negativo.");
+        }
+        this.salario = salario;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
 }
--- Cardealership_proyecto/src/modelo/persona/Vendedor.java
+++ Cardealership_proyecto/src/modelo/persona/Vendedor.java
@@ -1,5 +1,44 @@
 package modelo.persona;
+public class Vendedor extends Empleado {
 
-public class Vendedor {
+	   
+    private int vehiculosVendidos;
 
+    
+    public Vendedor(String tipoDocumento, String numeroDocumento, String nombre, String apellido, String telefono, float salario) {
+        super(tipoDocumento, numeroDocumento, nombre, apellido, telefono, salario);
+        this.vehiculosVendidos = 0; 
+    }
+
+   
+    public int getVehiculosVendidos() {
+        return vehiculosVendidos;
+    }
+
+ 
+    public void setVehiculosVendidos(int vehiculosVendidos) {
+        if (vehiculosVendidos < 0) {
+            throw new IllegalArgumentException("El número de vehículos vendidos no puede ser negativo.");
+        }
+        this.vehiculosVendidos = vehiculosVendidos;
+    }
+    
+    public void registrarVenta(Venta venta) {
+        if (venta == null) {
+            throw new IllegalArgumentException("La venta no puede ser nula.");
+        }
+        this.vehiculosVendidos++;
+        System.out.println("Venta registrada exitosamente por " + nombre + " " + apellido + ": " + venta);
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Vendedor{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", salario=" + getSalario() +
+                ", vehiculosVendidos=" + vehiculosVendidos +
+                '}';
+    }
 }
